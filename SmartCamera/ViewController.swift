0a1a6dff87ef9870aaa1cc933a9ef77ef2550484
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2018-11-14 12:26:12 +0100

    All done

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -2,19 +2,55 @@
 //  ViewController.swift
 //  SmartCamera
 //
-//  Created by De La Cruz, Eduardo on 14/11/2018.
+//  Created by De La Cruz, Eduardo on 13/11/2018.
 //  Copyright © 2018 De La Cruz, Eduardo. All rights reserved.
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Here is where we startup the camera
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        //TODO: set label for object detected and % of confidence
     }
-
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        // go to https://developer.apple.com/machine-learning/build-run-models/ and download the model and set it in model down
+        guard let model = try? VNCoreMLModel(for: VGG16().model) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, err) in
+            
+            // Perhaps check the err
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
 }
-
