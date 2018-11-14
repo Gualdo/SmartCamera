b76433d7b614ec028fb8c714940540881dbefadd
Eduardo De La Cruz <eduardo.delacruz@gft.com>
2018-11-14 12:20:58 +0100

    Initial Commit

---
+++ SmartCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartCamera
+//
+//  Created by De La Cruz, Eduardo on 14/11/2018.
+//  Copyright © 2018 De La Cruz, Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
