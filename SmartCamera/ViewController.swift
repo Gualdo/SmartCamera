0157072419f0a927c0f7e32cd4c6e2a7915c4764
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2018-11-15 18:18:04 +0100

    All done

--- SmartCamera/ViewController.swift
+++ SmartCamera/ViewController.swift
@@ -12,6 +12,17 @@ import Vision
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Start"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,12 +45,14 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
         
-        //TODO: set label for object detected and % of confidence
+        setupPercentageLabel()
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        // go to https://developer.apple.com/machine-learning/build-run-models/ and download the model and set it in model down
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // go to https://developer.apple.com/machine-learning/build-run-models/ and download the model and set it in model down VGG16 for VGG16 build model etc etc
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         guard let model = try? VNCoreMLModel(for: VGG16().model) else { return }
         
         let request = VNCoreMLRequest(model: model) { (finishedRequest, err) in
@@ -49,8 +62,24 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             guard let firstObservation = results.first else { return }
             
             print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                self.infoLabel.text = "The object detected is: \(firstObservation.identifier) with a confidence of \(firstObservation.confidence * 100)%"
+            }
         }
         
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+    private func setupPercentageLabel() {
+        view.addSubview(infoLabel)
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        infoLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        infoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        infoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        infoLabel.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
+//        infoLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
+        infoLabel.font = UIFont(descriptor: infoLabel.font.fontDescriptor, size: 20)
+        infoLabel.numberOfLines = 0
     }
 }
